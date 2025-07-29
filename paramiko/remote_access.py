36b3766f2cbb6a88ab1e17d9a5ebcf73f982e4d8
Bala Vignesh <82412177+balavi7@users.noreply.github.com>
2025-07-29 15:10:39 +0530

    Update remote_access.py

--- paramiko/remote_access.py
+++ paramiko/remote_access.py
@@ -3,7 +3,7 @@ import paramiko
 hostname = '13.233.98.222'
 port = 22
 username = 'ubuntu'
-key_file = '/mnt/d/Users/91966/Downloads/new_key.pem'
+key_file = '/Downloads/new_key.pem'
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
